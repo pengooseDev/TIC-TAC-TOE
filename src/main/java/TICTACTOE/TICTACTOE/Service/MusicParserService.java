1c01ef12ba05b4c22bbfe54fb44e8bcc5111c1a6
choincnp <whdudwns7777@gmail.com>
2023-01-31 20:07:24 +0900

    feat : new webconfig file

---
+++ src/main/java/TICTACTOE/TICTACTOE/Service/MusicParserService.java
@@ -0,0 +1,13 @@
+package TICTACTOE.TICTACTOE.Service;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping(value ="")
+public class MusicParserService {
+    public void jsonParsing(){
+
+    }
+}
